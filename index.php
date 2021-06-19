423572f04d155c0d65c433433122dc6e65064145
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-06-20 00:11:21 +0300

    More styling done to the index page. Animations added to the navigation bar. Logo added. Settings pushed to the bottom of the navigation bar. Overall, a lot of styling added :D.

--- index.php
+++ index.php
@@ -28,8 +28,38 @@ if (isset($_SESSION["userSignedIn"])) {
 
         <div id="application-section-container">
             <div id="application-nav-bar">
+                <div id="nav-bar-title">
+                    <img id="musafy-logo" src="./assets/img/musafy_logo.svg" alt="Musafy logo">
+                    <span id="nav-bar-title-text" class="nav-bar-text">Musafy</span>
+                </div>
 
+                <div id="nav-bar-home" class="nav-bar-item">
+                    <img id="nav-bar-home-img" class="nav-bar-img" src="./assets/img/nav_bar_home.svg" alt="Home">
+                    <span id="nav-bar-home-text" class="nav-bar-text">Home</span>
+                </div>
+
+                <div id="nav-bar-search" class="nav-bar-item">
+                    <img id="nav-bar-search-img" class="nav-bar-img" src="./assets/img/nav_bar_search.svg" alt="Search">
+                    <span id="nav-bar-search-text" class="nav-bar-text">Search</span>
+                </div>
+
+                <div id="nav-bar-browse" class="nav-bar-item">
+                    <img id="nav-bar-browse-img" class="nav-bar-img" src="./assets/img/nav_bar_browse.svg" alt="Browse">
+                    <span id="nav-bar-browse-text" class="nav-bar-text">Browse</span>
+                </div>
+
+                <div id="nav-bar-your-music" class="nav-bar-item">
+                    <img id="nav-bar-yourmusic-img" class="nav-bar-img" src="./assets/img/nav_bar_yourmusic.svg" alt="Your music">
+                    <span id="nav-bar-yourmusic-text" class="nav-bar-text">Your music</span>
+                </div>
+
+                <div id="nav-bar-settings" class="nav-bar-item">
+                    <img id="nav-bar-settings-img" class="nav-bar-img" src="./assets/img/nav_bar_settings.svg" alt="Settings">
+                    <span id="nav-bar-settings-text" class="nav-bar-text">Settings</span>
+                </div>
             </div>
+
+
             <div id="application-page">
 
             </div>
